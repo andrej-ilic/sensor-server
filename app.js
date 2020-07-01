b1291576d8d8fc2e4b36485536be5b91d12a76f6
Andrej <andrejilicb@gmail.com>
2020-07-01 11:24:05 +0200

    Add express and route to show recent logs

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const express = require("express");
+const readLastLines = require("read-last-lines");
+
 const Sensor = require("./Sensor");
 const SensorManager = require("./SensorManager");
 
@@ -7,3 +10,25 @@ const sm = new SensorManager(
 );
 
 sm.start();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const lineCount = Math.max(
+    Math.min(parseInt(req.query.lines) || 200, 1000),
+    1
+  );
+  const errorLogsOnly = req.query.errors === "1";
+
+  readLastLines
+    .read(
+      errorLogsOnly ? "./logs/ErrorLogs.log" : "./logs/CombinedLogs.log",
+      lineCount
+    )
+    .then((lines) =>
+      res.send(`<a href="?errors=1">Errors only</a><pre>${lines}</pre>`)
+    )
+    .catch((err) => res.send(err));
+});
+
+app.listen(process.env.PORT);
