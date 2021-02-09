ad229664230e294201e751a4d54eaec89d9fa856
Andrej Ilić <andrejilicb@gmail.com>
2021-02-09 17:43:36 +0100

    Change image url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ new SensorManager(
   new Sensor("mtiv09e1", "MTIV-09-E UNIC", "http://147.91.209.167")
 ).start();
 
-new ImageManager("http://147.91.209.67/slika.jpg", "image.jpg").start();
+new ImageManager("http://147.91.209.43/kamera.php", "image.jpg").start();
 
 const app = express();
 
